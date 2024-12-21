3529ed9960dd0c7415dd0a0f88acdb9d0de4ef9d
chaudharycoding <zaeemch78@gmail.com>
2024-12-20 22:50:56 -0500

    Usman talks too much

--- src/Wine_Quality/utils/common.py
+++ src/Wine_Quality/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from  import logger
+from Wine_Quality import logger
 import json
 import joblib
 from ensure import ensure_annotations
